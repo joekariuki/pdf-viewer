7e3eb0d2efd258df80bcb13be36a588ab053356a
Joe N Kariuki <joe.kariuki1@marist.edu>
2019-08-13 16:12:13 -0400

    Add get document and render page functions

--- js/main.js
+++ js/main.js
@@ -9,3 +9,14 @@ const scale   = 1.5,
       canvas  = document.querySelector('#pdf-render'),
       ctx     = canvas.getContext('2d');
 
+// Render the page
+const renderPage = num => {
+
+}
+
+//  Get Document
+pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
+  pdfDoc = pdfDoc_;
+
+  document.querySelector('#page-count').textContent = pdfDoc.numPages;
+});
