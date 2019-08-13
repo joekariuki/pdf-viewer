e7597488e2a9ec51e18c9a32424b9c9eb5ffa8f0
Joe N Kariuki <joe.kariuki1@marist.edu>
2019-08-13 16:03:39 -0400

    Add initial variables

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const url = '../docs/sample.pdf';
+
+let pdfDoc             = null,
+    pageNum            = 1,
+    pageIsRendering    = false,
+    pageIsNumIsPending = null;
+
+    const scale   = 1.5,
+          canvas  = document.querySelector('#pdf-render'),
+          ctx     = canvas.getContext('2d');
