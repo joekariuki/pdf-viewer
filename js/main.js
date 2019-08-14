64a3665657220a3566bd9bb68c4008dd4956e348
Joe N Kariuki <joe.kariuki1@marist.edu>
2019-08-14 11:15:55 -0400

    Add error message

--- js/main.js
+++ js/main.js
@@ -74,7 +74,16 @@ pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
   document.querySelector('#page-count').textContent = pdfDoc.numPages;
 
   renderPage(pageNum);
-});
+})
+  .catch(err => {
+    //   Display error
+    const div = document.createElement('div');
+    div.className = 'error';
+    div.appendChild(document.createTextNode(err.message));
+    document.querySelector('body').insertBefore(div, canvas);
+    // Remove topbar
+    document.querySelector('.top-bar').style.display = 'none';
+  })
 
 // Button events
 document.querySelector('#prev-page').addEventListener('click', showPrevPage);
